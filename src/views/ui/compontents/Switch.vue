690b33e1fd279e3d9b28eaf6ba46a74eb2e9a637
mayan <mayan0827l@163.com>
2022-06-22 17:22:18 +0800

    fix:ui组件样式调整m

--- src/views/ui/compontents/Switch.vue
+++ src/views/ui/compontents/Switch.vue
@@ -22,36 +22,6 @@
         <el-switch :value="true" disabled> </el-switch>
       </div>
     </div>
-    <!-- <el-row>
-      <el-col :span="1" class="switch-enable"><b>Enable</b></el-col>
-      <el-col :span="1">
-        <el-switch v-model="value1"> </el-switch>
-      </el-col>
-      <el-col :span="1">
-        <el-switch v-model="value"> </el-switch>
-      </el-col>
-      <el-col :span="1" class="small-switch">
-        <el-switch v-model="value2"> </el-switch>
-      </el-col>
-      <el-col :span="6" class="small-switch">
-        <el-switch v-model="value3"> </el-switch>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="1" class="switch-unable"><b>Unable</b></el-col>
-      <el-col :span="1">
-        <el-switch :value="false" disabled> </el-switch>
-      </el-col>
-      <el-col :span="1">
-        <el-switch :value="true" disabled> </el-switch>
-      </el-col>
-      <el-col :span="1" class="small-switch">
-        <el-switch :value="false" disabled> </el-switch>
-      </el-col>
-      <el-col :span="6" class="small-switch">
-        <el-switch :value="true" disabled> </el-switch>
-      </el-col>
-    </el-row> -->
   </div>
 </template>
 <script lang="ts">

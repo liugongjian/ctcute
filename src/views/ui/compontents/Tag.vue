9cbca3c0e7fdadc6bf1b80600cd720cb67619313
XinZhao <609014279@qq.com>
2022-06-24 17:17:48 +0800

    fix: 新增过滤改写到tag

--- src/views/ui/compontents/Tag.vue
+++ src/views/ui/compontents/Tag.vue
@@ -2,12 +2,11 @@
   <div class="sub-tag">
     <el-tag type="info">标签</el-tag>
     <el-tag type="info" closable>标签</el-tag>
-    <el-button class="button-new-tag" size="small">+ 标签</el-button>
+    <el-tag type="newtag">+ 新增过滤</el-tag>
     <el-tag type="danger">红色</el-tag>
     <el-tag type="success">绿色</el-tag>
     <el-tag type="">蓝色</el-tag>
-    <el-tag type="warning">橙色</el-tag>
-    <el-tag class="yellow-tag">黄色</el-tag>
+    <el-tag type="warning">黄色</el-tag>
   </div>
 </template>
 <script lang="ts">
@@ -28,20 +27,8 @@ export default class extends Vue {
 .sub-tag {
   margin-top: 24px;
 }
+
 .el-tag {
   margin-left: 10px;
-}
-.yellow-tag {
-  background-color: #fef6e7;
-  color: #fa9a34;
-  border: 1px solid #fcd68a;
-}
-.button-new-tag {
-  margin-left: 10px;
-  height: 22px;
-  line-height: 22px;
-  padding-top: 0;
-  padding-bottom: 0;
-  border: 1px dashed #ccc;
 }
 </style>

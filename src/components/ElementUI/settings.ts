c1dc57f21d60bfd651d4d7cad257a0ca3be0771d
胡佳婷 <hujt@chinatelecom.cn>
2022-07-01 10:34:00 +0800

    feat:给pagination加全局的popperClass,便于单独覆盖样式;抽离Element的全局样式配置

---
+++ src/components/ElementUI/settings.ts
@@ -0,0 +1,14 @@
+import ElementUI from 'element-ui'
+const Pagination = ElementUI.Pagination as any
+Pagination.props.layout = {
+  default: 'sizes,total, prev, pager, next'
+}
+Pagination.props.pageSize = {
+  default: 20
+}
+Pagination.props.pageSizes = {
+  default: [10, 20, 30, 40]
+}
+Pagination.props.popperClass = {
+  default: 'pagination__dropdown'
+}
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import ElementUIOverride from '@/components/ElementUI'
+import '@/components/ElementUI/settings'
 import i18n from '@/i18n'
 import mavonEditor from 'mavon-editor'
 import SvgIcon from 'vue-svgicon'
@@ -13,16 +14,6 @@ import './assets/css/element-variables.scss'
 import './assets/css/index.scss'
 import '@/permission'
 import './assets/fonts/iconfont.css'
-const Pagination = ElementUI.Pagination as any
-Pagination.props.layout = {
-  default: 'sizes,total, prev, pager, next'
-}
-Pagination.props.pageSize = {
-  default: 20
-}
-Pagination.props.pageSizes = {
-  default: [10, 20, 30, 40]
-}
 
 Vue.use(mavonEditor)
 Vue.use(ElementUI, { size: 'medium' })

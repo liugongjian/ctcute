1395731bb85f6aa967f160ef5de6236ce398c457
lizhiwei <lizhiwei@chinatelecom.cn>
2022-07-07 17:03:08 +0800

    feat：新增弹窗底页

--- src/views/page/Dialog/SuccessDialog.vue
+++ src/views/page/Dialog/SuccessDialog.vue
@@ -20,7 +20,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Prop } from 'vue-property-decorator'
 
 @Component({
   name: 'SuccessDialog',

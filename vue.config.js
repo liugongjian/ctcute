9d238aee3703fd68949dfc90a6cd38e663d2bca4
wanap <wanap@chinatelecom.cn>
2022-07-07 21:09:29 +0800

    svgIcon使用svg-sprite-loader + svgo-loader实现

--- vue.config.js
+++ vue.config.js
@@ -50,5 +50,36 @@ module.exports = {
         sassOptions: { outputStyle: 'expanded' }
       }
     }
+  },
+  chainWebpack: config => {
+    // set svg-sprite-loader
+    config.module
+      .rule("svg")
+      .exclude.add(path.join(__dirname, "src/assets/icons"))
+      .end();
+    config.module
+      .rule("icons")
+      .test(/\.svg$/)
+      .include.add(path.join(__dirname, "src/assets/icons"))
+      .end()
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
+      .options({
+        symbolId: "icon-[name]"
+      })
+      .end()
+      .use("svgo-loader")
+      .loader("svgo-loader")
+      .options({
+        plugins: [
+          {
+            name: 'removeAttrs',
+            params: {
+              attrs: '(fill|fill-rule)'
+            }
+          }
+        ]
+      })
+      .end()
   }
 }

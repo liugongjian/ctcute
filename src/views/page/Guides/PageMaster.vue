5f6a01a55a8a20d4f39fb5a6ddcfb759e524355b
qinruibin <1356278209@qq.com>
2022-07-12 18:52:17 +0800

    fix:修改页面版本

--- src/views/page/Guides/PageMaster.vue
+++ src/views/page/Guides/PageMaster.vue
@@ -16,34 +16,34 @@ export default class extends Vue {
 ## 页面维护人
 |   页面   |  维护人    |  版本    |
 | ---------------------------------------- | :---------: | :---------: |
-|   SimpleForm 基础表单    | 朱凌浩     |  v0.1    |
-|   ProForm1 复杂表单1    |  秦瑞斌    |  v0.1    |
-|   ProForm2 复杂表单2   |  秦瑞斌    |  v0.1    |
-|   StepForm 分步表单    |  何晋升    |   v0.1    |
-|   SmallDialog 小弹窗    |   黄璐璐    |   v0.1    |
-|   MediumDialog 中弹窗    |  秦瑞斌    |   v0.1    |
-|   LargeDialog 大弹窗    |   马妍   |  v0.1    |
-|   FullScreenDialog 全屏弹窗    |   吴博聪   |  v0.1    |
-|   ProSmallDialog 复杂小弹窗    |   吴博聪   |  v0.1    |
-|   ProMediumDialog 复杂中弹窗    |   吴博聪   |  v0.1    |
-|   SuccessDialog 成功弹窗    |   李志伟   |  v0.1    |
-|   WarnDialog 告警弹窗    |   李志伟   |  v0.1    |
-|   SimpleTable 基础表格    |   朱凌浩   |  v0.1    |
-|   ProTable1 复杂表格1    |   马妍   |  v0.1    |
-|   ProTable2 复杂表格2    |   马妍   |  v0.1    |
-|   ProTable3 复杂表格3    |   张晨曦   |  v0.1    |
-|   ProTable4 复杂表格4    |   肖仁   |  v0.1    |
-|   Card1 卡片1    |   王亚振   |  v0.1    |
-|   Card2 卡片2    |   孙善鹏   |  v0.1    |
-|   Card3 卡片3    |   赵丹   |  v0.1    |
-|   SimpleDetail 简单详情    |   胡佳婷   |  v0.1    |
-|   ProDetail1 复杂详情1    |   赵昕   |  v0.1    |
-|   ProDetail2 复杂详情2    |   朱玉豆   |  v0.1    |
-|   SimpleChart 基础图表    |   孙善鹏   |  v0.1    |
-|   Page403 403页    |   万璐敏   |  v0.1    |
-|   Page404 404页    |   万璐敏   |  v0.1    |
-|   Page500 500页    |   万璐敏   |  v0.1    |
-|   EmptyPage 空页面    |   万璐敏   |  v0.1    |
+|   SimpleForm 基础表单    | 朱凌浩     |  v1.0    |
+|   ProForm1 复杂表单1    |  秦瑞斌    |  v1.0    |
+|   ProForm2 复杂表单2   |  秦瑞斌    |  v1.0    |
+|   StepForm 分步表单    |  何晋升    |   v1.0    |
+|   SmallDialog 小弹窗    |   黄璐璐    |   v1.0    |
+|   MediumDialog 中弹窗    |  秦瑞斌    |   v1.0    |
+|   LargeDialog 大弹窗    |   马妍   |  v1.0    |
+|   FullScreenDialog 全屏弹窗    |   吴博聪   |  v1.0    |
+|   ProSmallDialog 复杂小弹窗    |   吴博聪   |  v1.0    |
+|   ProMediumDialog 复杂中弹窗    |   吴博聪   |  v1.0    |
+|   SuccessDialog 成功弹窗    |   李志伟   |  v1.0    |
+|   WarnDialog 告警弹窗    |   李志伟   |  v1.0    |
+|   SimpleTable 基础表格    |   朱凌浩   |  v1.0    |
+|   ProTable1 复杂表格1    |   马妍   |  v1.0    |
+|   ProTable2 复杂表格2    |   马妍   |  v1.0    |
+|   ProTable3 复杂表格3    |   张晨曦   |  v1.0    |
+|   ProTable4 复杂表格4    |   肖仁   |  v1.0    |
+|   Card1 卡片1    |   王亚振   |  v1.0    |
+|   Card2 卡片2    |   孙善鹏   |  v1.0    |
+|   Card3 卡片3    |   赵丹   |  v1.0    |
+|   SimpleDetail 简单详情    |   胡佳婷   |  v1.0    |
+|   ProDetail1 复杂详情1    |   赵昕   |  v1.0    |
+|   ProDetail2 复杂详情2    |   朱玉豆   |  v1.0    |
+|   SimpleChart 基础图表    |   孙善鹏   |  v1.0    |
+|   Page403 403页    |   万璐敏   |  v1.0    |
+|   Page404 404页    |   万璐敏   |  v1.0    |
+|   Page500 500页    |   万璐敏   |  v1.0    |
+|   EmptyPage 空页面    |   万璐敏   |  v1.0    |
 `
 }
 </script>

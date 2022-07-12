7c1315a46ef14794ccb8b3085291d0ba6a239fd8
qinruibin <1356278209@qq.com>
2022-07-12 19:00:37 +0800

    fix:更改组件版本号

--- src/views/page/Guides/ComponentMaster.vue
+++ src/views/page/Guides/ComponentMaster.vue
@@ -16,38 +16,38 @@ export default class extends Vue {
 ## 组件维护人
 |   组件   |  维护人    |  版本    |
 | ---------------------------------------- | :---------: | :---------: |
-|   Button 按钮    | 马妍     |  v0.1    |
-|   TextButton 文字按钮    |  马妍    |  v0.1    |
-|   IconButton 图标按钮   |  马妍    |  v0.1    |
-|   Content Area Tips 内容区提示    |  马妍    |   v0.1    |
-|   Link 链接    |   马妍    |   v0.1    |
-|   Breadcrumb 面包屑    |  马妍    |   v0.1    |
-|   Pagination 分页器    |   马妍、胡佳婷   |  v0.1    |
-|   Steps 步骤条    |    吴静  |   v0.1   |
-|   Input 输入框    |    马妍    |   v0.1     |
-|   Select 选择器    |   马妍    |   v0.1     |
-|   Radio 单选框    |   马妍    |   v0.1     |
-|   Checkbox 多选框    |   马妍    |   v0.1     |
-|   Rate 评分    |   胡佳婷   |   v0.1   |
-|   Slider 滑动输入条    |   马妍    |   v0.1     |
-|   Upload 上传     |   马妍    |   v0.1     |
-|   Switch 开关    |  马妍    |   v0.1     |
-|   Popover 气泡提示     |  马妍    |   v0.1     |
-|   Tooltip 文字提示     |   马妍    |   v0.1     |
-|   Tabs 选项卡     |  马妍    |   v0.1     |
-|   SelectedButton 选择按钮     |  马妍、王亚振    |  v0.1     |
-|   Table 表格     |  朱玉豆    |   v0.1    |
-|   Alert 警告     |  赵昕    |  v0.1    |
-|   Content Area Tips 内容区提示     |   马妍    |   v0.1    |
-|   Tag 标签     |   赵昕    |  v0.1    |
-|   Tree 树形菜单     |  肖仁    |  v0.1    |
-|   State 状态      |   王亚振   |   v0.1   |
-|   Notification 通知提醒框      |   马妍    |   v0.1    |
-|   Toast 轻提示      |   赵昕    |  v0.1    |
-|   Drawer 抽屉      |   赵昕    |  v0.1    |
-|   Transfer 穿梭框      |   赵昕    |  v0.1    |
-|   Collapse 折叠       |   赵昕    |  v0.1    |
-|   Timeline 时间轴       |  赵昕    |  v0.1    |
+|   Button 按钮    | 马妍     |  v1.0    |
+|   TextButton 文字按钮    |  马妍    |  v1.0    |
+|   IconButton 图标按钮   |  马妍    |  v1.0    |
+|   Content Area Tips 内容区提示    |  马妍    |   v1.0    |
+|   Link 链接    |   马妍    |   v1.0    |
+|   Breadcrumb 面包屑    |  马妍    |   v1.0    |
+|   Pagination 分页器    |   马妍、胡佳婷   |  v1.0    |
+|   Steps 步骤条    |    吴静  |   v1.0   |
+|   Input 输入框    |    马妍    |   v1.0     |
+|   Select 选择器    |   马妍    |   v1.0     |
+|   Radio 单选框    |   马妍    |   v1.0     |
+|   Checkbox 多选框    |   马妍    |   v1.0     |
+|   Rate 评分    |   胡佳婷   |   v1.0   |
+|   Slider 滑动输入条    |   马妍    |   v1.0     |
+|   Upload 上传     |   马妍    |   v1.0     |
+|   Switch 开关    |  马妍    |   v1.0     |
+|   Popover 气泡提示     |  马妍    |   v1.0     |
+|   Tooltip 文字提示     |   马妍    |   v1.0     |
+|   Tabs 选项卡     |  马妍    |   v1.0     |
+|   SelectedButton 选择按钮     |  马妍、王亚振    |  v1.0     |
+|   Table 表格     |  朱玉豆    |   v1.0    |
+|   Alert 警告     |  赵昕    |  v1.0    |
+|   Content Area Tips 内容区提示     |   马妍    |   v1.0    |
+|   Tag 标签     |   赵昕    |  v1.0    |
+|   Tree 树形菜单     |  肖仁    |  v1.0    |
+|   State 状态      |   王亚振   |   v1.0   |
+|   Notification 通知提醒框      |   马妍    |   v1.0    |
+|   Toast 轻提示      |   赵昕    |  v1.0    |
+|   Drawer 抽屉      |   赵昕    |  v1.0    |
+|   Transfer 穿梭框      |   赵昕    |  v1.0    |
+|   Collapse 折叠       |   赵昕    |  v1.0    |
+|   Timeline 时间轴       |  赵昕    |  v1.0    |
 `
 }
 </script>

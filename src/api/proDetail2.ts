7b500740740539e9ddb8288264dddf3e9edb4880
朱玉豆 <zhuyd@chinatelecom.cn>
2022-07-21 13:17:02 +0800

    fix: 复杂详情页控制台警告

--- src/api/proDetail2.ts
+++ src/api/proDetail2.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: 朱玉豆
+ * @Date: 2022-07-08 17:04:20
+ * @LastEditors: 朱玉豆
+ * @LastEditTime: 2022-07-21 13:16:19
+ * @Description:
+ */
 import request from '@/utils/request'
 import { AxiosPromise } from 'axios'
 import * as ProDetail2 from '@/types/ProDetail2'
@@ -8,6 +15,6 @@ import * as ProDetail2 from '@/types/ProDetail2'
  */
 export const getProDetail = (): AxiosPromise<ProDetail2.ProDetailInfo2> =>
   request({
-    url: '/mock/175/detail/proDetail',
+    url: '/mock/175/detail/proDetail2',
     method: 'get',
   })

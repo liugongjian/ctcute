682e31963213dc024fc3690de128e167c0d8fcba
wangyazhen <603684240@qq.com>
2022-07-22 15:51:14 +0800

    fix： 补充文档信息，card 样式调整

--- src/views/component/Base/CuteEditInput/index.vue
+++ src/views/component/Base/CuteEditInput/index.vue
@@ -12,6 +12,16 @@
       <component-demo path="@/views/component/Base/CuteEditInput/Demo1.vue">
         <demo1 />
       </component-demo>
+
+      <h2>CuteEditInput Attributes</h2>
+      <el-table :data="prop" fit border>
+        <el-table-column prop="name" label="参数" />
+        <el-table-column prop="desc" label="说明" />
+        <el-table-column prop="type" label="类型" />
+        <el-table-column prop="required" label="必/选填" />
+        <el-table-column prop="options" label="可选值" />
+        <el-table-column prop="default" label="默认值" />
+      </el-table>
     </div>
   </div>
 </template>
@@ -27,5 +37,24 @@ import ComponentDemo from '@/layout/components/ComponentDemo/index.vue'
     Demo1,
   },
 })
-export default class extends Vue {}
+export default class extends Vue {
+  private prop = [
+    {
+      name: 'v-model',
+      desc: '绑定值',
+      type: 'String',
+      required: '必填',
+      options: null,
+      default: null,
+    },
+    {
+      name: 'edit-input-save',
+      desc: '点确定按钮时 emit 事件',
+      type: 'callback',
+      required: '选填',
+      options: null,
+      default: null,
+    },
+  ]
+}
 </script>

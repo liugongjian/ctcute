d2c01b1e4c027aa3eec029f69dfd335ea79d2e50
Linghao Zhu <>
2022-07-22 11:51:27 +0800

    feat: 更新package base

---
+++ packages/base/index.ts
@@ -0,0 +1,71 @@
+/*
+ * @Author: huanglulu
+ * @Date: 2022-07-18 15:05:01
+ * @LastEditors: huanglulu
+ * @LastEditTime: 2022-07-21 17:22:27
+ * @Description:
+ */
+import ElementUI from 'element-ui'
+import ElementUIOverride from './components/ElementUI'
+import './components/ElementUI/settings'
+import './assets/css/element-variables.scss'
+import './assets/fonts/iconfont.css'
+import './assets/icons/index'
+
+import CuteChart from './components/CuteCharts/CuteChart.vue'
+import CuteChartCard from './components/CuteCharts/CuteChartCard.vue'
+import CuteButtonGroup from './components/CuteCombinationButton/CuteButtonGroup.vue'
+import CuteEditInput from './components/CuteEditInput/index.vue'
+import CuteFormInfo from './components/CuteFormInfo/index.vue'
+import CuteRemindInput from './components/CuteInput/CuteRemindInput.vue'
+import CuteSpecialRadio from './components/CuteRadio/CuteSpecialRadio.vue'
+import CuteRemindSelect from './components/CuteSelect/CuteRemindSelect.vue'
+import CuteSelectedInput from './components/CuteSelect/CuteSelectedInput.vue'
+import CuteState from './components/CuteState/index.vue'
+import CuteStepsMulti from './components/CuteSteps/CuteStepsMulti.vue'
+import CuteSortTable from './components/CuteTable/CuteSortTable.vue'
+import CuteTableFilter from './components/CuteTable/CuteTableFilter.vue'
+import CuteTag from './components/CuteTag/index.vue'
+import CuteTitledBlock from './components/CuteTitledBlock/index.vue'
+
+export {
+  CuteChart,
+  CuteChartCard,
+  CuteButtonGroup,
+  CuteEditInput,
+  CuteFormInfo,
+  CuteRemindInput,
+  CuteSpecialRadio,
+  CuteRemindSelect,
+  CuteSelectedInput,
+  CuteState,
+  CuteStepsMulti,
+  CuteSortTable,
+  CuteTableFilter,
+  CuteTag,
+  CuteTitledBlock,
+}
+
+/**
+ * 仅注册使用频率高的，轻量级的组件
+ */
+const componentsList = [
+  CuteFormInfo,
+  CuteEditInput,
+  CuteTag,
+  CuteRemindInput,
+  CuteRemindSelect,
+  CuteSelectedInput,
+  CuteSortTable,
+]
+
+export default {
+  install(Vue) {
+    Vue.use(ElementUI, { size: 'medium' })
+    Vue.use(ElementUIOverride)
+
+    componentsList.map((component: any) => {
+      Vue.component(component.options.name, component)
+    })
+  },
+}
--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui'
-import ElementUIOverride from '@cutedesign/base/components/ElementUI'
-import CuteComponent from '@cutedesign/base/components/index'
+import CuteComponent from '@cutedesign/base'
 import i18n from '@/i18n'
 import mavonEditor from 'mavon-editor'
 import '@/permission'
-import '@cutedesign/base/components/ElementUI/settings'
-import '@cutedesign/base/assets/css/element-variables.scss'
-import '@cutedesign/base/assets/fonts/iconfont.css'
-// svgIcon
-import '@cutedesign/base/assets/icons/index'
 import 'mavon-editor/dist/css/index.css'
 import './assets/css/index.scss'
 
 Vue.use(mavonEditor)
-Vue.use(ElementUI, { size: 'medium' })
-Vue.use(ElementUIOverride)
 Vue.use(CuteComponent)
 
 Vue.config.productionTip = false

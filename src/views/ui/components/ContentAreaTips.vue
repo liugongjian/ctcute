2cdd9c8ff983906e476245208db54985262d3543
XinZhao <609014279@qq.com>
2022-07-26 15:14:54 +0800

    fix: Alert组件新增属性content-area-tip区分类型

--- src/views/ui/components/ContentAreaTips.vue
+++ src/views/ui/components/ContentAreaTips.vue
@@ -2,12 +2,14 @@
   <div class="sub-contentareatips">
     <p>在内容区域的提示样式，在内容下方与内容进行左对齐，与内容间距为8px。后面直接跟一个提示icon的情况</p>
     <div class="contentareatips-content">
-      <el-alert title="这是一个普通提示" :closable="false">
+      <el-alert title="这是一个普通提示" :closable="false" content-area-tip>
         <span slot="icon-custom"><svg-icon class="el-alert__icon" name="info-circle" /></span>
       </el-alert>
-      <el-alert title="这是一个警告提示" type="warning" show-icon :closable="false"> </el-alert>
-      <el-alert title="这是一个错误提示" type="error" show-icon :closable="false"> </el-alert>
-      <el-alert show-icon :closable="false">
+      <el-alert title="这是一个警告提示" type="warning" show-icon :closable="false" content-area-tip>
+      </el-alert>
+      <el-alert title="这是一个错误提示" type="error" show-icon :closable="false" content-area-tip>
+      </el-alert>
+      <el-alert show-icon :closable="false" content-area-tip>
         <span slot="icon-custom">
           <el-tooltip effect="dark" placement="top" content="提示描述性文案">
             <svg-icon class="el-alert__icon" name="question-circle" />

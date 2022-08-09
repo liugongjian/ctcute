61b7742b0371fcce736b759b0256733c8c9b4173
胡佳婷 <hujt@chinatelecom.cn>
2022-08-09 08:21:45 +0800

    feat:补充CuteTitledBlock的组件说明

--- src/views/component/Base/CuteTitledBlock/index.vue
+++ src/views/component/Base/CuteTitledBlock/index.vue
@@ -2,7 +2,7 @@
  * @Author: 胡佳婷
  * @Date: 2022-07-21 09:23:09
  * @LastEditors: 胡佳婷
- * @LastEditTime: 2022-07-21 10:27:13
+ * @LastEditTime: 2022-08-09 08:18:30
  * @Description:
 -->
 <template>
@@ -13,6 +13,19 @@
       <component-demo path="@/views/component/Base/CuteTitledBlock/Demo1.vue">
         <demo1 />
       </component-demo>
+      <h2>CuteTitledBlock Attributes</h2>
+      <el-table :data="prop" fit border>
+        <el-table-column prop="name" label="参数" />
+        <el-table-column prop="desc" label="说明" />
+        <el-table-column prop="type" label="类型" />
+        <el-table-column prop="options" label="可选值" />
+        <el-table-column prop="default" label="默认值" />
+      </el-table>
+      <h2>CuteTitledBlock Slot</h2>
+      <el-table :data="slot" fit border>
+        <el-table-column prop="name" label="参数" />
+        <el-table-column prop="desc" label="说明" />
+      </el-table>
     </div>
   </div>
 </template>
@@ -28,5 +41,25 @@ import ComponentDemo from '@/layout/components/ComponentDemo/index.vue'
     Demo1,
   },
 })
-export default class extends Vue {}
+export default class extends Vue {
+  private prop = [
+    {
+      name: 'title',
+      desc: '标题',
+      type: 'String',
+      options: null,
+      default: null,
+    },
+  ]
+  private slot = [
+    {
+      name: 'content',
+      desc: '块的内容',
+    },
+    {
+      name: 'extra',
+      desc: '【可选】标题右侧额外的内容，一般是操作',
+    },
+  ]
+}
 </script>

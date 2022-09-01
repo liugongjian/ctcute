9cce827c7038959847f8c6ed386f1015d5d8f4d8
王月功 <wangyuegong@chinatelecom.cn>
2022-09-01 10:03:36 +0800

    fix：重置 config

--- public/config/index.js
+++ public/config/index.js
@@ -2,7 +2,7 @@ window.commonConfig = {
   env: 'local',
   // authenticateType: 'iam',
   // baseUrl: 'https://vip.ctcdn.cn',
-  authenticateType: 'iam',
+  authenticateType: 'local',
   enableAuthorize: true,
   authorizeType: 'local',
   baseUrl: 'http://localhost:7002',

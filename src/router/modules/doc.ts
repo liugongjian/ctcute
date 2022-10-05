93ab486e99973357c9a6cab5167e49f4a2f3ca12
胡佳婷 <hujt@chinatelecom.cn>
2022-10-05 09:38:27 +0800

    feat:增加更换主题的说明和更新日志

--- src/router/modules/doc.ts
+++ src/router/modules/doc.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: 胡佳婷
+ * @Date: 2022-10-04 17:01:49
+ * @LastEditors: 胡佳婷
+ * @LastEditTime: 2022-10-05 09:20:35
+ * @Description:
+ */
 import Layout from '@/layout/doc.vue'
 
 const docRoutes = [
@@ -34,6 +41,25 @@ const docRoutes = [
         component: () => import('@/views/doc/Guides/QuickStart.vue'),
         meta: {
           title: '快速上手',
+          breadcrumb: true,
+        },
+      },
+    ],
+  },
+  // 更换主题
+  {
+    path: '/docs/theme',
+    component: Layout,
+    meta: {
+      breadcrumb: false,
+    },
+    children: [
+      {
+        path: '',
+        name: 'Theme',
+        component: () => import('@/views/doc/Guides/Theme.vue'),
+        meta: {
+          title: '更换主题',
           breadcrumb: true,
         },
       },

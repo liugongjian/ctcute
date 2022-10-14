dbe0e907d499d1380183eca2b987f258be191be7
秦瑞斌 <1356278209@qq.com>
2022-10-14 11:17:21 +0800

    feat:新增三列展示列表

--- src/views/ui/components/Table.vue
+++ src/views/ui/components/Table.vue
@@ -643,7 +643,98 @@
       >
       </el-pagination>
     </div>
+    <h3>详情页基本信息展示列表</h3>
+    <div class="sub-table-horizon">
+      <el-descriptions :column="3" border>
+        <el-descriptions-item>
+          <template slot="label"> 内部标识符 </template>
+          DE34237632363
+        </el-descriptions-item>
 
+        <el-descriptions-item>
+          <template slot="label"> 标识符 </template>
+          GMSFZ
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label"> 标识符 </template>
+          GMSFZ
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label"> 中文名称 </template>
+          公民身份证
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label"> 英文名称 </template>
+          gong-min-shen-fen-zheng
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label"> 英文名称</template>
+          gong-min-shen-fen-zheng
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label"> 语境 </template>
+          天翼云公司
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label"> 版本 </template>
+          1.0
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label"> 版本 </template>
+          1.0
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label"> 同义名称 </template>
+          BB
+        </el-descriptions-item>
+      </el-descriptions>
+    </div>
+    <div class="sub-table-horizon">
+      <el-descriptions :column="2" border>
+        <el-descriptions-item>
+          <template slot="label"> 内部标识符 </template>
+          DE34237632363
+        </el-descriptions-item>
+
+        <el-descriptions-item>
+          <template slot="label"> 标识符 </template>
+          GMSFZ
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label"> 中文名称 </template>
+          公民身份证
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label"> 英文名称 </template>
+          gong-min-shen-fen-zheng
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label"> 语境 </template>
+          天翼云公司
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label"> 版本 </template>
+          1.0
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label"> 同义名称 </template>
+          BB
+        </el-descriptions-item>
+      </el-descriptions>
+    </div>
+    <div class="sub-table-horizon" border>
+      <el-table :data="tableData" :span-method="row">
+        <el-table-column prop="dataFilter" label="数据筛选" width="230" align="center"> </el-table-column>
+        <el-table-column prop="dataTime" label="数据时间字段" align="center"> </el-table-column>
+        <el-table-column prop="key" label="主键" align="center" width="260"> </el-table-column>
+        <el-table-column label="标签" align="center">
+          <el-table-column label="标签单元" prop="tag1" align="center"> </el-table-column>
+          <el-table-column label="标签集名称" prop="tag2" align="center"> </el-table-column>
+          <el-table-column label="标签名称" prop="tag3" align="center"> </el-table-column>
+          <el-table-column label="规则描述" prop="tag4" align="center" width="230"> </el-table-column>
+        </el-table-column>
+      </el-table>
+    </div>
     <h3>带列设置的表格</h3>
     <div class="sub-table-settings">
       <!-- 列设置选择表单 -->
@@ -1090,6 +1181,10 @@ export default class extends Vue {
 .sub-table-horizon {
   border-bottom: 1px solid $color-grey-7;
   padding-bottom: 24px;
+
+  ::v-deep .is-group {
+    line-height: 40px;
+  }
 }
 
 .sub-index {

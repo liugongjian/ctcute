961054b01e08ca5b1a28fd53ef17839d7389ca89
秦瑞斌 <1356278209@qq.com>
2022-10-14 16:55:55 +0800

    feat:新增最佳实践

--- src/views/doc/BestPractice/CodeMirror.vue
+++ src/views/doc/BestPractice/CodeMirror.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: 胡佳婷
  * @Date: 2022-10-08 10:43:23
- * @LastEditors: 胡佳婷
- * @LastEditTime: 2022-10-08 12:29:51
+ * @LastEditors: 秦瑞斌
+ * @LastEditTime: 2022-10-14 15:06:41
  * @Description:
 -->
 <template>
@@ -27,9 +27,66 @@ export default class extends Vue {
 "vue-codemirror": "^4.0.6",
 
 ### 推荐主题
-
+ 解决普通文本编辑器当中不能输入tab键、回车乱跳、皮肤难看的问题。
+ 我们选择了三种不同的代码主题来适配不同的业务场景 ：base16-light，elegant，base16-dark
 ### 代码示例
+需要在vue-codemirror中引入codemirror
+还需要引入codemirror的一些配置
 \`\`\`
+import { codemirror } from 'vue-codemirror'
+//三种主题颜色
+import 'codemirror/theme/base16-dark.css' 
+import 'codemirror/theme/base16-light.css'
+import 'codemirror/theme/elegant.css'
+//支持的语言
+import 'codemirror/mode/javascript/javascript.js'
+import 'codemirror/mode/css/css.js'
+import 'codemirror/mode/xml/xml.js'
+import 'codemirror/mode/clike/clike.js'
+import 'codemirror/mode/markdown/markdown.js'
+import 'codemirror/mode/python/python.js'
+import 'codemirror/mode/r/r.js'
+import 'codemirror/mode/shell/shell.js'
+import 'codemirror/mode/sql/sql.js'
+import 'codemirror/mode/swift/swift.js'
+import 'codemirror/mode/vue/vue.js'
+
+import 'codemirror/addon/selection/active-line' //光标行背景高亮，配置里面也需要styleActiveLine设置为true
+import 'codemirror/mode/css/css' //必须要引入的样式
+
+
+
+\`\`\`
+在模板中使用
+\`\`\`
+<codemirror v-model="Code" :options=“options"></codemirror>
+\`\`\`
+v-model为绑定的代码块 options为编辑器的配置部分
+\`\`\`
+  private Code = 'function findSequence(goal) {
+  function find(start, history) {
+    if (start == goal)
+      return history;
+    else if (start > goal)
+      return null;
+    else
+      return find(start + 5, "(" + history + " + 5)") ||
+        find(start * 3, "(" + history + " * 3)");
+  }
+  return find(1, "1");
+}'
+  private options = {
+   tabSize: 2, // 缩进格式
+    mode: 'text/javascript', //实现JavaScript代码高亮 这里可进行引入的切换
+    theme: 'base16-light', // 主题，对应主题库 JS 需要提前引入
+    lineNumbers: true, // 显示行号
+    viewportMargin: Infinity, //处理高度自适应时搭配使用
+    line: true, // 开启语法检查
+    styleActiveLine: true, // 高亮选中行
+    hintOptions: {
+      completeSingle: true, // 当匹配只有一项的时候是否自动补全
+    },
+ }
 
 \`\`\`
 `
--- src/views/doc/BestPractice/RichText.vue
+++ src/views/doc/BestPractice/RichText.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: 胡佳婷
  * @Date: 2022-10-08 10:43:23
- * @LastEditors: 胡佳婷
- * @LastEditTime: 2022-10-08 10:54:02
+ * @LastEditors: 秦瑞斌
+ * @LastEditTime: 2022-10-14 15:13:41
  * @Description:
 -->
 <template>
@@ -28,11 +28,79 @@ export default class extends Vue {
 "@wangeditor/editor-for-vue": "^1.0.1"
 
 ### 推荐主题
-
+——轻量级web富文本编辑器
+wangEditor是用javascript编写的 轻量级web富文本编辑器 ，依赖于jQuery和fontAwesome字体库， 支持所有浏览器 。使用 wangEditor 可以轻松创建web富文本框，并可以自定义扩展菜单功能。
 ### 代码示例
+引入工具栏和编辑器注册
 \`\`\`
+import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
+ components: { Editor, Toolbar },
+\`\`\`
+模板中使用
+\`\`\`
+<!-- 工具栏 -->
+<Toolbar style="border-bottom: 1px solid #ccc" :editor="editor" :default-config="toolbarConfig" />
+<!-- 编辑器 -->
+<Editor
+  v-model="html"
+  style="height: 400px; overflow-y: hidden"
+  :default-config="editorConfig"
+  @onChange="onChange"
+  @onCreated="onCreated"
+/>
 
 \`\`\`
+配置信息
+\`\`\`
+ editor: null,
+ html: '<p>hello&nbsp;world</p>',
+ toolbarConfig: {
+    // toolbarKeys: [ /* 显示哪些菜单，如何排序、分组 */ ],
+    // excludeKeys: [ /* 隐藏哪些菜单 */ ],
+ },
+ editorConfig: { 
+    placeholder: '请输入内容...',
+    // 所有的菜单配置，都要在 MENU_CONF 属性下
+    MENU_CONF: {
+      //配置上传图片
+		uploadImage: {
+		customUpload: this.uploadImg
+		}
+	}
+},
+\`\`\`
+全局引入样式
+\`\`\`
+<style src="@wangeditor/editor/dist/css/style.css"></style>
+\`\`\`
+调用方法
+\`\`\`
+//上传图片
+uploadImg(file, insertFn){
+  let imgData = new FormData();
+	mgData.append('file', file);
+  //调接口，上传图片
+  testUpImg(imgData).then(response => {
+    if(response.data.code == 0){
+      insertFn(response.data.data.url);
+    }else{
+    }
+  })
+},
+onCreated(editor) {
+  this.editor = Object.seal(editor) // 一定要用 Object.seal() ，否则会报错
+},
+//清空
+clearForm(){
+  this.html = '';
+},
+beforeDestroy() {
+  const editor = this.editor
+  if (editor == null) return
+     editor.destroy() // 组件销毁时，及时销毁编辑器
+},
+\`\`\`
+
 `
 }
 </script>

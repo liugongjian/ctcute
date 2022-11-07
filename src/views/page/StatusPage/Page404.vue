1f82476bada79fcc33c9285e24907b8e17593158
秦瑞斌 <1356278209@qq.com>
2022-11-07 08:58:15 +0800

    fix:状态页调整

--- src/views/page/StatusPage/Page404.vue
+++ src/views/page/StatusPage/Page404.vue
@@ -1,15 +1,24 @@
 <!--
  * @Author: 万璐敏
  * @Date: 2022-07-08 13:45:09
- * @LastEditors: 万璐敏
- * @LastEditTime: 2022-07-18 14:05:44
+ * @LastEditors: 秦瑞斌
+ * @LastEditTime: 2022-11-07 08:57:02
  * @Description: 404页
 -->
 <template>
   <div class="page-status">
     <div class="box">
-      <img src="./Image/page-404.svg" />
-      <div class="text">抱歉，您访问的页面不存在</div>
+      <el-empty :image="Img" :image-size="400" description="抱歉，您访问的页面不存在"></el-empty>
     </div>
   </div>
 </template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import Img from './Image/page-404.svg'
+@Component({
+  name: 'Page404',
+})
+export default class extends Vue {
+  private Img = Img
+}
+</script>

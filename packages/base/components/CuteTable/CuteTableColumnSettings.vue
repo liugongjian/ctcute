4f84c8a574c7fdfd84457d8690701fc0fdadbd31
胡一苗 <huyimiao@huyimiaodeMacBook-Pro-2.local>
2022-12-23 18:22:20 +0800

    fix: 修改CuteTableColumnSettings样式

--- packages/base/components/CuteTable/CuteTableColumnSettings.vue
+++ packages/base/components/CuteTable/CuteTableColumnSettings.vue
@@ -1,62 +1,34 @@
 <template>
-  <div>
+  <div class="table-column-settings">
     <!-- 列设置选择表单 -->
-    <!-- <el-popover
+    <el-popover
       placement="bottom-end"
       trigger="click"
-      class="table-column-settings__popover"
+      popper-class="table-column-settings__popover"
       @show="handlePopoverShow"
       @hide="handlePopoverHide"
-    > -->
-      <!-- 固定的，用来全选 -->
-      <!-- <el-checkbox v-model="allSelected" label="全选" @change="handleSelectedChange"></el-checkbox> -->
-      <!-- 分割线 -->
-      <!-- <el-divider class="table-column-settings__divider"></el-divider> -->
-      <!-- 列出全部选项 -->
-      <!-- <el-checkbox-group v-model="selectedColumns" class="table-column-settings__label-group">
-        <el-checkbox v-for="v in tableColumns" :key="v.label" :label="v.label" :disabled="v.isDisabled">
-        </el-checkbox>
-      </el-checkbox-group> -->
-
-      <!-- 列设置图标 -->
-      <!-- <div slot="reference" :class="['head-title', { 'table-column-settings__svg--active': popoverShow }]">
-        <svg-icon name="setting" width="14" height="14" class="table-column-settings__svg" />
-        <span>列设置</span>
-      </div>
-    </el-popover> -->
-
-    <!-- el-dropdown改写el-popover -->
-    <el-dropdown 
-      trigger="click"
-      class="table-column-settings__dropdown"
-      :hide-on-click="false"
-      @visible-change="handleDropdownVisible"
     >
-      <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item>
-          <el-checkbox v-model="allSelected" label="全选" @change="handleSelectedChange">
-          </el-checkbox>
-        </el-dropdown-item>
-        <!-- 分割线 -->
-        <el-divider class="table-column-settings__divider"></el-divider>
-        <el-checkbox-group v-model="selectedColumns" class="table-column-settings__label-group">
-          <el-dropdown-item v-for="v in tableColumns">
-            <el-checkbox  :key="v.label" :label="v.label" :disabled="v.isDisabled">
-            </el-checkbox>
-          </el-dropdown-item>
-        </el-checkbox-group>
-      </el-dropdown-menu>
-
-      <div :class="['head-title', { 'table-column-settings__svg--active': dropdownShow }]">
-        <svg-icon name="setting" width="14" height="14" :class="['table-column-settings__svg', { 'table-column-settings__svg--active': dropdownShow }]" />
-        <span>列设置</span>
+      <!-- 固定的，用来全选 -->
+      <el-checkbox v-model="allSelected" label="全选" @change="handleSelectedChange" />
+      <!-- 分割线 -->
+      <el-divider class="table-column-settings__popover__divider" />
+      <!-- 列出全部选项 -->
+      <el-checkbox-group v-model="selectedColumns" class="table-column-settings__popover__label-group">
+        <el-checkbox v-for="v in tableColumns" :key="v.label" :label="v.label" :disabled="v.isDisabled" />
+      </el-checkbox-group>
+      <!-- 列设置图标 -->
+      <div slot="reference" class="table-column-settings__btns">
+        <el-button type="text" plain>
+          <svg-icon name="setting" />
+          列设置
+        </el-button>
       </div>
-    </el-dropdown>
+    </el-popover>
     <!-- 表格 -->
     <el-table
       ref="tableRef"
       v-loading="tableHook.loading"
-      :data="tableData? tableData: tableHook.tableData"
+      :data="tableHook.tableData"
       fit
       border
       v-bind="$attrs"
@@ -89,7 +61,6 @@
 import { Vue, Component, Prop, Watch, Ref } from 'vue-property-decorator'
 import { ElTable } from 'element-ui/types/table'
 import TableHookClass from '@cutedesign/base/hook/TableHook'
-import { camelCase } from 'yargs-parser'
 
 @Component({
   name: 'CuteTableColumnSettings',
@@ -99,13 +70,10 @@ export default class extends Vue {
   @Prop({ type: Array, default: [] }) tableColumns?: []
 
   // 收集选中的选项中的 label 属性
-  private selectedColumns = ['主机别名', '实例状态', '操作']
+  private selectedColumns = []
 
   // popover 状态变量
   private popoverShow = false
-
-  // popover 状态变量
-  private dropdownShow = false
 
   // 全选状态
   private allSelected = false
@@ -138,11 +106,6 @@ export default class extends Vue {
 
   public handlePopoverHide() {
     this.popoverShow = false
-  }
-
-  public handleDropdownVisible(val) {
-    console.log('handleDropdownVisible', val)
-    this.dropdownShow = val
   }
 
   /**
@@ -199,7 +162,6 @@ export default class extends Vue {
   }
 
   mounted() {
-    console.log('mounted', this.tableData)
     this.tableHook = new TableHookClass({}, this.innerGetTable, this.tableRef, false)
     this.tableHook.query()
     this.initSelectedColumns()
@@ -209,67 +171,10 @@ export default class extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.el-dropdown-menu {
-  width: 190px;
-  height: 341px;
-  overflow: hidden;
-
-  ::v-deep .el-checkbox__input.is-checked + .el-checkbox__label {
-    color:black !important;
-  }
-
-  ::v-deep .el-dropdown-menu__item {
-    &:hover {
-      .el-checkbox__label {
-        color: $color-master-1 !important;
-      }
-    }
-  }
-}
-
 .table-column-settings {
-  // &__popover {
-  //   float: right;
-  //   cursor: pointer;
-
-  //   .head-title {
-  //     margin-bottom: 13px;
-  //   }
-  // }
-  &__dropdown {
-    float: right;
-    cursor: pointer;
-    .head-title {
-      margin-bottom: 13px;
-      &:hover {
-        color: $color-master-3;
-        .table-column-settings__svg {
-          color: $color-master-3;
-        }
-      }
-    }
-  }
-
-  &__divider {
-    margin: 10px 0 !important;
-  }
-
-  &__label-group {
-    max-height: 300px;
-    overflow: auto;
-
-    label {
-      margin-bottom: 10px;
-      display: block;
-    }
-  }
-
-  &__svg {
-    margin-right: 6px;
-  }
-
-  &__svg--active {
-    color: $color-master-3;
+  &__btns {
+    display: flex;
+    justify-content: right;
   }
 
   &-paginationContainer {
@@ -278,6 +183,41 @@ export default class extends Vue {
 
   &__page {
     float: right;
+  }
+}
+</style>
+<style lang="scss">
+// TODO 讨论：自定义全局样式
+.table-column-settings__popover {
+  min-width: 190px;
+  padding: 0;
+
+  &__divider {
+    margin: 0;
+  }
+
+  &__label-group {
+    max-height: 300px;
+    overflow: auto;
+  }
+
+  label.el-checkbox {
+    display: block;
+    margin: 5px 0;
+    height: 30px;
+    line-height: 30px;
+
+    .el-checkbox__inner {
+      margin-left: 12px;
+    }
+
+    &:hover:not(.is-disabled) {
+      background-color: $color-master-7;
+
+      .el-checkbox__label {
+        color: $color-master-1;
+      }
+    }
   }
 }
 </style>
--- src/views/ui/components/Table.vue
+++ src/views/ui/components/Table.vue
@@ -739,70 +739,42 @@
     </div>
     <h3>带列设置的表格</h3>
     <div class="sub-table-settings">
-      <el-dropdown 
-        trigger="click"
-        class="table-column-settings__dropdown"
-        :hide-on-click="false"
-        @visible-change="handleDropdownVisible"
+      <cute-table-column-settings
+        :table-data="tableComponentData && tableComponentData.tableData10"
+        :table-columns="tableComponentColumns"
       >
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>
-            <el-checkbox v-model="allSelected" label="全选" @change="handleSelectedChange">
-            </el-checkbox>
-          </el-dropdown-item>
-          <!-- 分割线 -->
-          <el-divider class="table-column-settings__divider"></el-divider>
-          <el-checkbox-group v-model="selectedOptions" class="table-column-settings__label-group">
-            <el-dropdown-item v-for="v in allOptions">
-              <el-checkbox  :key="v.label" :label="v.label" :disabled="v.disabled">
-              </el-checkbox>
-            </el-dropdown-item>
-          </el-checkbox-group>
-        </el-dropdown-menu>
-
-        <div :class="['head-title', { 'table-column-settings__svg--active': dropdownShow }]">
-          <svg-icon name="setting" width="14" height="14" :class="['table-column-settings__svg', { 'table-column-settings__svg--active': dropdownShow }]" />
-          <span>列设置</span>
-        </div>
-      </el-dropdown>
-
-      <!-- 表格 -->
-      <el-table :data="tableComponentData && tableComponentData.tableData10" fit border>
-        <el-table-column v-for="v in selectedOptionsWithProp" :key="v.prop" :prop="v.prop" :label="v.label">
-          <template slot-scope="{ row }">
-            <span v-if="v.prop === 'healthy'" class="health-state">
-              <span class="health-dot" :class="`health-dot--${row.healthy}`" />{{ HEALTH[row.healthy] }}
-            </span>
-
-            <span v-else-if="v.prop === 'name'">
-              <span class="text-ellipsis name-primary" style="width: 100%">{{ row.name }}</span>
-            </span>
-
-            <span v-else-if="v.prop === 'label'">
-              <el-tag type="info" size="small" style="margin-right: 8px">{{ row.label[0] }}</el-tag>
-              <el-tag type="info" size="small">{{ row.label[1] }}</el-tag>
-            </span>
-
-            <span v-else>{{ row[v.prop] }}</span>
-          </template>
-        </el-table-column>
-
-        <el-table-column label="操作">
-          <template>
+        <template #name="{ scope }">
+          <span class="text-ellipsis name-primary" style="width: 100%">{{ scope.row.name }}</span>
+        </template>
+        <template #label="{ scope }">
+          <el-tag
+            v-for="item of scope.row.label"
+            :key="item"
+            type="info"
+            size="small"
+            style="margin-right: 8px"
+          >
+            {{ item }}
+          </el-tag>
+        </template>
+        <template #description="{ scope }">
+          <el-tooltip class="item" effect="dark" :content="scope.row.description" placement="top">
+            <span class="text-ellipsis">{{ scope.row.description }}</span>
+          </el-tooltip>
+        </template>
+        <template #healthy="{ scope }">
+          <div class="health-state">
+            <span class="health-dot" :class="`health-dot--${scope.row.healthy}`" />
+            <span>{{ HEALTH[scope.row.healthy] }}</span>
+          </div>
+        </template>
+        <template #operation="{}">
+          <div>
             <el-button type="text" size="small" class="bt-operation">卸载</el-button>
             <el-button type="text" size="small" class="bt-operation">扩容</el-button>
-          </template>  
-        </el-table-column>
-      </el-table>
-      <!-- 分页 -->
-      <el-pagination
-        class="pagination"
-        :current-page="currentPage4"
-        :total="100"
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-      >
-      </el-pagination>
+          </div>
+        </template>
+      </cute-table-column-settings>
     </div>
   </div>
 </template>
@@ -840,6 +812,39 @@ export default class extends Vue {
     { prop: 'healthy', label: '健康状态', slot: 'healthy' },
     { prop: 'operation', label: '操作', width: 190, slot: 'operation' },
   ]
+
+  private tableComponentColumns = [
+    { prop: 'name', label: '主机别名', isSelected: true, isDisabled: true, slot: 'name' },
+    { prop: 'status', label: '实例状态', isSelected: true, isDisabled: true },
+    { prop: 'ip', label: 'IP地址', isSelected: true, isDisabled: false },
+    { prop: 'time', label: '时间', isSelected: true, isDisabled: false, props: { width: 150 } },
+    {
+      prop: 'label',
+      label: '标签',
+      isSelected: true,
+      isDisabled: false,
+      slot: 'label',
+      props: { width: 150 },
+    },
+    {
+      prop: 'description',
+      label: '描述',
+      isSelected: false,
+      isDisabled: false,
+      slot: 'description',
+      props: { width: 150 },
+    },
+    { prop: 'healthy', label: '健康状态', isSelected: true, isDisabled: false, slot: 'healthy' },
+    {
+      prop: 'operation',
+      label: '操作',
+      isSelected: true,
+      isDisabled: true,
+      slot: 'operation',
+      props: { width: 190 },
+    },
+  ]
+
   private HEALTH = HEALTH
   private flag = false
 
@@ -1008,134 +1013,6 @@ export default class extends Vue {
   private handleClick(index, row) {
     console.log(index, row)
   }
-
-  // popver 状态变量
-  private popverShow = false
-
-  private dropdownShow = false
-
-  public handlePopverShow() {
-    this.popverShow = true
-  }
-
-  public handlePopverhide() {
-    this.popverShow = false
-  }
-
-  public handleDropdownVisible(val) {
-    console.log('handleDropdownVisible', val)
-    this.dropdownShow = val
-  }
-
-  // 全选状态变量
-  private allSelected = false
-
-  // 判断是否已全选
-  public isAllSelected() {
-    if (this.selectedOptions.length === this.allOptions.length) {
-      this.allSelected = true
-    } else {
-      this.allSelected = false
-    }
-  }
-
-  // 全选按钮 全选事件
-  // 根据全选状态变量，来判断将要做什么
-  public handleSelectedChange() {
-    const data = []
-    if (!this.allSelected) {
-      this.allOptions.forEach((v: any) => {
-        if (this.disabledOptions.some(item => item === v.label)) {
-          data.push(v.label)
-        }
-      })
-    } else {
-      this.allOptions.forEach((v: any) => {
-        data.push(v.label)
-      })
-    }
-    this.selectedOptions = data
-  }
-
-  private allOptions = [
-    {
-      label: '主机别名',
-      prop: 'name',
-      disabled: true,
-    },
-    {
-      label: '实例状态',
-      prop: 'status',
-      disabled: true,
-    },
-    {
-      label: 'IP地址',
-      prop: 'ip',
-      disabled: false,
-    },
-    {
-      label: '时间',
-      prop: 'time',
-      disabled: false,
-    },
-    {
-      label: '标签',
-      prop: 'label',
-      disabled: false,
-    },
-    {
-      label: '描述',
-      prop: 'description',
-      disabled: false,
-    },
-    {
-      label: '健康状态',
-      prop: 'healthy',
-      disabled: false,
-    },
-  ]
-  private selectedOptions = ['主机别名', '实例状态', 'IP地址', '时间', '健康状态']
-  private selectedOptionsWithProp = [
-    {
-      label: '主机别名',
-      prop: 'name',
-    },
-    {
-      label: '实例状态',
-      prop: 'status',
-    },
-    {
-      label: 'IP地址',
-      prop: 'ip',
-    },
-    {
-      label: '时间',
-      prop: 'time',
-    },
-    {
-      label: '健康状态',
-      prop: 'healthy',
-    },
-  ]
-
-  // 监听selectedOptions，判断是否需要修改全选按钮
-  @Watch('selectedOptions')
-  onChangeValue() {
-    this.isAllSelected() // 判断是否已全选
-    this.syncSelectedOptions() // 同步
-  }
-
-  private syncSelectedOptions() {
-    const data = []
-    this.allOptions.forEach((v: { label: string; prop: string }) => {
-      if (this.selectedOptions.some(item => item === v.label)) {
-        data.push({ label: v.label, prop: v.prop })
-      }
-    })
-    this.selectedOptionsWithProp = data
-  }
-
-  private disabledOptions = ['主机别名', '实例状态']
 
   private STATUS = STATUS
 }
@@ -1365,78 +1242,6 @@ export default class extends Vue {
     &--5 {
       background: $disabled-color;
     }
-  }
-}
-
-.el-dropdown-menu {
-  width: 190px;
-  height: 341px;
-  overflow: hidden;
-
-  ::v-deep .el-checkbox__input.is-checked + .el-checkbox__label {
-    color:black !important;
-  }
-
-  ::v-deep .el-dropdown-menu__item {
-    &:hover {
-      .el-checkbox__label {
-        color: $color-master-1 !important;
-      }
-    }
-  }
-}
-
-.table-column-settings {
-  // &__popover {
-  //   float: right;
-  //   cursor: pointer;
-
-  //   .head-title {
-  //     margin-bottom: 13px;
-  //   }
-  // }
-  &__dropdown {
-    float: right;
-    cursor: pointer;
-    .head-title {
-      margin-bottom: 13px;
-      &:hover {
-        color: $color-master-3;
-        .table-column-settings__svg {
-          color: $color-master-3;
-        }
-      }
-    }
-  }
-
-  &__divider {
-    margin: 10px 0 !important;
-  }
-
-  &__label-group {
-    max-height: 300px;
-    overflow: auto;
-
-    label {
-      margin-bottom: 10px;
-      display: block;
-    }
-  }
-
-  &__svg {
-    margin-right: 6px;
-  }
-
-  &__svg--active {
-    color: $color-master-3;
-  }
-
-  &-paginationContainer {
-    height: 20px;
-  }
-
-  &__page {
-    float: right;
   }
 }
 

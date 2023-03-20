e7f6e41a0dc973a7bf77d11df0732d1b2574e1d5
Linghao Zhu <>
2023-03-20 10:37:36 +0800

    feat: 更新Button Test页面

--- src/views/component/Test/Button.vue
+++ src/views/component/Test/Button.vue
@@ -9,6 +9,8 @@
   <div>
     <h1>Button</h1>
     <ButtonDemo />
+    <TextButtonDemo />
+    <IconButtonDemo />
     <el-row>
       <h3>默认按钮</h3>
       <el-button>默认按钮</el-button>
@@ -137,11 +139,15 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import ButtonDemo from '@/views/ui/components/Button.vue'
+import TextButtonDemo from '@/views/ui/components/TextButton.vue'
+import IconButtonDemo from '@/views/ui/components/IconButton.vue'
 
 @Component({
   name: 'Button',
   components: {
     ButtonDemo,
+    TextButtonDemo,
+    IconButtonDemo,
   },
 })
 export default class extends Vue {}

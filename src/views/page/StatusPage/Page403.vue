5188cd43ed059fd87801655791d55ff68dc51c29
yueyue0827 <1423411336@qq.com>
2023-03-29 14:17:32 +0800

    fix:修改状态页面样式

--- src/views/page/StatusPage/Page403.vue
+++ src/views/page/StatusPage/Page403.vue
@@ -8,13 +8,21 @@
 <template>
   <div class="page-status">
     <div class="box">
-      <el-empty :image="Img" :image-size="400" description="抱歉，您没有访问权限"></el-empty>
+      <!-- <el-empty :image="Img" :image-size="400" description="抱歉，您没有访问权限"></el-empty> -->
+      <div class="box-image">
+        <el-image :src="Img" fit="cover"></el-image>
+      </div>
+      <div class="box-text">
+        <div class="box-text-title">403</div>
+        <div class="box-text-tips">你无权访问该页面</div>
+      </div>
     </div>
   </div>
 </template>
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
-import Img from './Image/page-403.svg'
+// import Img from './Image/page-403.svg'
+import Img from './Image/page-error.svg'
 @Component({
   name: 'Page403',
 })
--- src/views/page/StatusPage/Page404.vue
+++ src/views/page/StatusPage/Page404.vue
@@ -8,13 +8,21 @@
 <template>
   <div class="page-status">
     <div class="box">
-      <el-empty :image="Img" :image-size="400" description="抱歉，您访问的页面不存在"></el-empty>
+      <!-- <el-empty :image="Img" :image-size="400" description="抱歉，您访问的页面不存在"></el-empty> -->
+      <div class="box-image">
+        <el-image :src="Img" fit="cover"></el-image>
+      </div>
+      <div class="box-text">
+        <div class="box-text-title">404</div>
+        <div class="box-text-tips">抱歉，您访问的页面不存在</div>
+      </div>
     </div>
   </div>
 </template>
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
-import Img from './Image/page-404.svg'
+// import Img from './Image/page-404.svg'
+import Img from './Image/page-error.svg'
 @Component({
   name: 'Page404',
 })

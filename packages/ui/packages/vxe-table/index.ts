4075ce00db7d19cacf41196a3d4b391638b2ec73
wanap <wanap@chinatelecom.cn>
2023-04-10 18:27:33 +0800

    refactor:多包合并到ui包，并支持tree-shaking

---
+++ packages/ui/packages/vxe-table/index.ts
@@ -0,0 +1,24 @@
+import VXETable from 'vxe-table'
+VXETable.setup({
+  icon: {
+    TABLE_RADIO_CHECKED: 'cute-icon-selected',
+    TABLE_RADIO_UNCHECKED: 'cute-icon-not-selected',
+    TABLE_EDIT: 'cute-icon-edit',
+    TABLE_EXPAND_OPEN: 'cute-icon-caret-down',
+    TABLE_EXPAND_CLOSE: 'cute-icon-caret-right',
+  },
+})
+
+VXETable.setup({
+  table: {
+    rowConfig: {
+      isHover: true,
+    },
+  },
+})
+
+export default {
+  install(Vue) {
+    Vue.use(VXETable)
+  }
+}

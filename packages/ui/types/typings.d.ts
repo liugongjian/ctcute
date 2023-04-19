198ad205b96b9d538df679884461e3ce333a2817
胡佳婷 <hujt@chinatelecom.cn>
2023-04-19 08:10:23 +0800

    fix:ui包导出element上的实例,打包时候遗漏待补充

--- packages/ui/types/typings.d.ts
+++ packages/ui/types/typings.d.ts
@@ -8,29 +8,7 @@
 export * from 'element-ui/types/element-ui'
 
 import * as ElementUI from 'element-ui/types/element-ui'
-import { CuteAppendix } from './CuteAppendix'
-import { CuteArea } from './CuteArea'
-import { CuteButtonGroup } from './CuteButtonGroup'
-import { CuteChart } from './CuteChart'
-import { CuteDistributionBar } from './CuteDistributionBar'
-import { CuteEditInput } from './CuteEditInput'
-import { CuteFormInfo } from './CuteFormInfo'
-import { CuteLayout } from './CuteLayout'
-import { CuteRemindInput } from './CuteRemindInput'
-import { CuteRemindSelect } from './CuteRemindSelect'
-import { CuteSelectedInput } from './CuteSelectedInput'
-import { CuteSelectTree } from './CuteSelectTree'
-import { CuteSlider } from './CuteSlider'
-import { CuteSortTable } from './CuteSortTable'
-import { CuteSpecialRadio } from './CuteSpecialRadio'
-import { CuteState } from './CuteState'
-import { CuteStepsMulti } from './CuteStepsMulti'
-import { CuteTableColumnSettings } from './CuteTableColumnSettings'
-import { CuteTableFilter } from './CuteTableFilter'
-import { CuteTag } from './CuteTag'
-import { CuteTitledBlock } from './CuteTitledBlock'
-
-
+export * from './cute'
 
 export default ElementUI
 

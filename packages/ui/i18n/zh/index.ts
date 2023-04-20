38195bbfff6b056641084ae0432c9cfdb3b0d82e
Linghao Zhu <>
2023-04-20 17:09:48 +0800

    feat: 修复colorpicker错误

--- packages/ui/i18n/zh/index.ts
+++ packages/ui/i18n/zh/index.ts
@@ -9,6 +9,10 @@ const zh: { [key: string]: any } = {
       total: '共{total}条',
       pageClassifier: '页',
     },
+    colorpicker: {
+      clear: '取 消',
+      confirm: '确 定',
+    },
   },
 }
 

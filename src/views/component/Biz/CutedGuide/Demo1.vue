b06d8e26320a2c3f24ce85402c1c03376719be2d
zhangkj5 <zhangkj5@chinatelecom.cn>
2023-05-05 10:46:00 +0800

    feat: 引导组件新增业务场景demo

--- src/views/component/Biz/CutedGuide/Demo1.vue
+++ src/views/component/Biz/CutedGuide/Demo1.vue
@@ -27,10 +27,34 @@
     <br />
     <br />
     <el-button id="step_1" type="primary" @click="startGuide">开始引导</el-button>
-    <el-button id="step_2" @click="startGuide">Upload</el-button>
-    <el-button id="step_3" type="primary" @click="startGuide">Save</el-button>
-    <el-button id="step_4" @click="startGuide">...</el-button>
+    <el-button id="step_2">Upload</el-button>
+    <el-button id="step_3" type="primary">Save</el-button>
+    <el-button id="step_4">...</el-button>
     <Guide ref="guide" :steps="steps" />
+    <br />
+    <br />
+    <el-button id="step_1_auto" type="primary" @click="startAutoGuide">自动演示填写表单</el-button>
+    <br />
+    <br />
+    <div id="step_1_auto_fix" :style="{ width: '720px' }">
+      <el-form class="simple-form">
+        <el-form-item id="step_2_auto" label="姓名">
+          <el-input v-model="form.name" placeholder="请输入姓名" />
+        </el-form-item>
+        <el-form-item id="step_3_auto" label="年龄">
+          <el-input v-model="form.age" placeholder="请输入年龄" />
+        </el-form-item>
+        <el-form-item id="step_4_auto" label="性别">
+          <el-select v-model="form.sex">
+            <el-option :value="0" label="男" />
+            <el-option :value="1" label="女" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="">
+          <el-button id="step_5_auto" type="primary">提交</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -114,6 +138,12 @@ export default class extends Vue {
     ],
   }
 
+  private form = {
+    name: '',
+    age: '',
+    sex: '',
+  }
+
   /**
    * 开始引导
    */
@@ -161,6 +191,94 @@ export default class extends Vue {
             descriptionTitle: '描述标题4',
             descriptionDetails: '描述内容4',
             position: 'top',
+          },
+        },
+      ]
+    })
+  }
+
+  private startAutoGuide() {
+    const driver = this.guideRef.getCtx({
+      opacity: 0.2,
+      allowClose: false,
+      padding: 5,
+      isHasPopover: false,
+    })
+    const inputEvt = new InputEvent('input', {
+      inputType: 'insertText',
+      dataTransfer: null,
+      isComposing: false,
+    })
+    this.$nextTick(() => {
+      this.steps = [
+        {
+          element: '#step_1_auto',
+          popover: {
+            totalSteps: '2',
+            currentStep: '1',
+            descriptionTitle: '自动演示',
+            descriptionDetails: '点击开始演示',
+            position: 'right',
+            nextBtnText: '开始演示',
+          },
+        },
+        {
+          element: '#step_1_auto_fix',
+          popover: {
+            totalSteps: '2',
+            currentStep: '1',
+            descriptionTitle: '自动演示',
+            descriptionDetails: '演示中...',
+            position: 'right',
+            disable: true,
+            nextBtnText: '演示中',
+          },
+          onHighlighted: () => {
+            setTimeout(() => {
+              const curDom = document.querySelector('#step_2_auto')
+              driver.highlight(curDom)
+            }, 500)
+            setTimeout(() => {
+              const curDom = document.querySelector('#step_2_auto .el-input__inner') as HTMLInputElement
+              curDom.value = '张xx'
+              curDom.dispatchEvent(inputEvt)
+            }, 1000)
+            setTimeout(() => {
+              const curDom = document.querySelector('#step_3_auto')
+              driver.highlight(curDom)
+            }, 1500)
+            setTimeout(() => {
+              const curDom = document.querySelector('#step_3_auto .el-input__inner') as HTMLInputElement
+              curDom.value = '24'
+              curDom.dispatchEvent(inputEvt)
+            }, 2000)
+            setTimeout(() => {
+              const curDom = document.querySelector('#step_4_auto .el-input__inner')
+              driver.highlight(curDom)
+            }, 2500)
+            setTimeout(() => {
+              const curDom = document.querySelector('#step_4_auto .el-input__inner') as HTMLInputElement
+              curDom.click()
+            }, 3000)
+            setTimeout(() => {
+              const curDom = document.querySelector('#step_4_auto .el-input__inner') as HTMLInputElement
+              curDom.click()
+              curDom.value = '男'
+              curDom.blur()
+              driver.reset()
+            }, 3500)
+            setTimeout(() => {
+              this.guideRef.driver.moveNext()
+            }, 4000)
+          },
+        },
+        {
+          element: '#step_5_auto',
+          popover: {
+            totalSteps: '2',
+            currentStep: '2',
+            descriptionTitle: '自动演示',
+            descriptionDetails: '演示结束',
           },
         },
       ]

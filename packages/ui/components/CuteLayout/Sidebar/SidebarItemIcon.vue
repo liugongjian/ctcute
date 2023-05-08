d0259faa88aeed05399876b2c80a800b7c8dc0d3
Linghao Zhu <>
2023-05-08 13:50:10 +0800

    feat: 抽象出SidebarItemIcon.vue组件

---
+++ packages/ui/components/CuteLayout/Sidebar/SidebarItemIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg-icon v-if="isSvgIcon" :name="name" class="menu-item-icon" />
+  <i v-else class="menu-item-icon" :class="name"></i>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'SidebarItemIcon',
+})
+export default class extends Vue {
+  @Prop() public name: string
+  @Prop({ default: 'svg' }) public iconType: string
+
+  private get isSvgIcon() {
+    return this.iconType === 'svg' && !this.name.startsWith('el-')
+  }
+}
+</script>

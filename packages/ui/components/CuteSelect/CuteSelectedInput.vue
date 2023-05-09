3110959358f415c8f2f2e38bc52da938b866118e
孙善鹏 <6699384@qq.com>
2023-05-09 18:15:13 +0800

    fix:cuteSelectedInput 增加size控制，input增加配置错误的时候的默认值，增加一个upload demo

--- packages/ui/components/CuteSelect/CuteSelectedInput.vue
+++ packages/ui/components/CuteSelect/CuteSelectedInput.vue
@@ -2,12 +2,12 @@
  * @Author: 马妍
  * @Date: 2022-07-14 19:41:25
  * @LastEditors: 孙善鹏
- * @LastEditTime: 2023-05-09 16:47:28
+ * @LastEditTime: 2023-05-09 17:49:50
  * @Description: 操作已选项
 -->
 <template>
   <div class="cute-selected-input">
-    <el-select ref="select" :value="''" placeholder="" :size="size" @change="handelSelectChange">
+    <el-select ref="select" :value="''" placeholder="" size="medium" @change="handelSelectChange">
       <span slot="prefix" class="text" :class="flag ? 'select-field' : 'select-uncheck'">{{ sele }}</span>
       <el-option
         v-for="(v, i) in options"
@@ -31,7 +31,7 @@ import Locale from '@cutedesign/ui/mixins/locale'
 export default class extends Mixins(Locale) {
   @Prop({ type: Array, default: [] }) checkedList?: [] //已选中数据
   @Prop({ type: Array, default: [] }) options?: [] //下拉数据
-  @Prop({ type: String, default: 'mormal' }) size?: 'mormal' //size
+  @Prop({ type: String, default: 'medium' }) size?: 'medium' //size
 
   private get flag() {
     return this.checkedList.length > 0

3b4c258245ba37b7ca75afc0bf3868f656d06940
王月功 <wangyuegong@chinatelecom.cn>
2023-05-13 11:03:28 +0800

    feat:调整 i18n 回退方案，支持按照 cute 的设置覆写默认表现，可侵入到 element-ui 原组件中

---
+++ packages/ui/i18n/zh/element-ui.ts
@@ -0,0 +1,24 @@
+/**
+ * 所有原 element-ui 组件（未被覆写）所需中文语言包在此定义
+ */
+import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
+
+export default {
+  el: {
+    ...zhLocale.el,
+    pagination: {
+      goto: '前往',
+      pagesize: '',
+      total: '2共{total}条',
+      pageClassifier: '页',
+    },
+    messagebox: {
+      confirm: '确 认',
+      cancel: '取 消',
+    },
+    colorpicker: {
+      clear: '清 空',
+      confirm: '确 定',
+    },
+  },
+}

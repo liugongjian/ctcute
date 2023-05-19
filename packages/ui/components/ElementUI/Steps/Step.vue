03d0c242b593323c897d5c58ec8d9440487a83a0
黄靖 <huangj14@chinatelecom.cn>
2023-05-19 20:20:00 +0800

    feat: StepForm增加提交成功/失败icon；修复Steps自适应宽度问题，StepForm步骤条居中

--- packages/ui/components/ElementUI/Steps/Step.vue
+++ packages/ui/components/ElementUI/Steps/Step.vue
@@ -10,7 +10,11 @@
     class="el-step"
   >
     <!-- icon & line -->
-    <div :class="`is-${currentStatus}`" class="el-step__head">
+    <div :class="`is-${currentStatus}`" class="el-step__head" :style="{
+          minWidth: !isSimple && !isCenter && !isVertical && $refs.title
+            ? $refs.title.offsetLeft + $refs.title.offsetWidth + 'px' : undefined
+        }"
+      >
       <div
         class="el-step__line"
         :style="{
@@ -132,8 +136,30 @@ export default {
         isSimple,
         $parent: { space },
       } = this
+      console.log(this.isLast, isSimple ? '' : space)
       return isSimple ? '' : space
     },
+    //  原始逻辑
+    //   style: function() {
+    //   const style = {};
+    //   const parent = this.$parent;
+    //   const len = parent.steps.length;
+
+    //   const space = (typeof this.space === 'number'
+    //     ? this.space + 'px'
+    //     : this.space
+    //       ? this.space
+    //       : 100 / (len - (this.isCenter ? 0 : 1)) + '%');
+    //   style.flexBasis = space;
+    //   if (this.isVertical) return style;
+    //   if (this.isLast) {
+    //     style.maxWidth = 100 / this.stepsCount + '%';
+    //   } else {
+    //     style.marginRight = -this.$parent.stepOffset + 'px';
+    //   }
+
+    //   return style;
+    // }
 
     style() {
       const style = {}

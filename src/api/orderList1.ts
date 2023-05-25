852b786c2f8e817adb106b5b6a353eac2588f3be
weix10 <weix10@chinatelecom.cn>
2023-05-25 10:08:03 +0800

    fix:修复订购页样式变量替换

---
+++ src/api/orderList1.ts
@@ -0,0 +1,12 @@
+import request from '@/utils/request'
+import { AxiosPromise } from 'axios'
+import * as OrderTabel from '@/types/OrderTable1'
+
+
+
+export const getOrderTableList = (params: OrderTabel.TableParams): AxiosPromise<OrderTabel.TableData> =>
+  request({
+    url: '/mock/175/order/list',
+    method: 'get',
+    data: params,
+  })

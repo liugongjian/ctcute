9c6a2e9e7793cfe0088f6caa850f5850e4b89b3c
liugj <liugj@chinatelecom.cn>
2023-05-29 15:26:52 +0800

    'feat:添加type'

--- packages/ui/types/CuteSelectedInput.d.ts
+++ packages/ui/types/CuteSelectedInput.d.ts
@@ -1,10 +1,26 @@
 /*
  * @Author: 胡佳婷
  * @Date: 2023-04-18 20:34:19
- * @LastEditors: 胡佳婷
- * @LastEditTime: 2023-04-18 20:34:28
+ * @LastEditors: liugj
+ * @LastEditTime: 2023-05-29 15:19:33
  * @Description:
  */
 import { CuteComponent } from './component'
 
-export declare class CuteSelectedInput extends CuteComponent {}
+interface CuteSelectedInputOption {
+  label: String
+  value: String
+}
+export declare class CuteSelectedInput extends CuteComponent {
+  /* 已选中数据 */
+  checkedList: Array[String]
+
+  /* 已选中数据 */
+  options: CuteSelectedInputOption[]
+
+  /* 组件尺寸大小 */
+  size: String
+
+  /* 下拉框change事件回调 */
+  handelSelectChange: Function
+}

9deda5c14837c2fd2d94983d9a5e890d5f42c9f6
lix_1423411336 <1423411336@qq.com>
2023-05-30 09:48:20 +0800

    feat:添加加载失败页面

---
+++ src/views/page/StatusPage/LoadError.vue
@@ -0,0 +1,33 @@
+<!--
+ * @Author: 李悦
+ * @Date: 2022-07-08 13:45:09
+ * @LastEditors: 李悦
+ * @LastEditTime: 2022-11-07 08:53:25
+ * @Description: 空页面
+-->
+<template>
+  <div class="page-status">
+    <div class="box-new">
+      <div class="box-image">
+        <el-image :src="Img" fit="cover"></el-image>
+      </div>
+      <div class="box-text">
+        <div class="box-text-tips">页面加载失败，请刷新后重试</div>
+        <el-button class="box-button" type="primary" @click="refresh">刷新</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({
+  name: 'LoadError',
+})
+export default class extends Vue {
+  private Img = require('./Image/load-error.svg')
+  private refresh() {
+    location.reload()
+  }
+}
+</script>

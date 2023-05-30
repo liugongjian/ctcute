ed5b034976f8fa89af4309b36e2bac72076f6c90
黄靖 <huangj14@chinatelecom.cn>
2023-05-30 19:42:06 +0800

    feat: 补充Types

--- packages/ui/types/CuteFixedFooter.d.ts
+++ packages/ui/types/CuteFixedFooter.d.ts
@@ -2,9 +2,14 @@
  * @Author: 黄靖
  * @Date: 2023-04-25 17:49:09
  * @LastEditors: 黄靖
- * @LastEditTime: 2023-04-25 17:49:09
+ * @LastEditTime: 2023-05-30 19:16:03
  * @Description:
  */
 import { CuteComponent } from './component'
 
-export declare class CuteFixedFooter extends CuteComponent {}
+export interface CuteFixedFooterSlots {
+  default: VNode[]
+}
+export declare class CuteFixedFooter extends CuteComponent {
+  $slots: CuteFixedFooterSlots
+}

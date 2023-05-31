7db460ff25db9589509999bd56d33dcbfef6e54f
孙善鹏 <6699384@qq.com>
2023-05-31 15:30:39 +0800

    fix:修改textarea表单错误时背景颜色

--- src/views/ui/components/Input.vue
+++ src/views/ui/components/Input.vue
@@ -333,6 +333,9 @@
         <el-form-item label="输入查询" prop="name" :size="inputSize">
           <el-input v-model="form.name" placeholder="请输入策略名称" />
         </el-form-item>
+        <el-form-item label="输入查询" prop="name2" :size="inputSize">
+          <el-input v-model="form.name2" type="textarea" placeholder="请输入策略名称" />
+        </el-form-item>
         <el-form-item label="" :size="inputSize">
           <el-button type="primary" @click="submit">确 定</el-button>
         </el-form-item>
@@ -353,6 +356,7 @@ export default class extends Vue {
   // 表单校验规则
   private rules = {
     name: [{ required: true, message: '请输入名称', trigger: 'blur' }],
+    name2: [{ required: true, message: '请输入名称', trigger: 'blur' }],
   }
   // 表单Ref对象
   @Ref('simpleForm')
@@ -361,6 +365,7 @@ export default class extends Vue {
   // 表单对象
   private form = {
     name: '',
+    name2: '',
   }
   /**
    * 提交表单

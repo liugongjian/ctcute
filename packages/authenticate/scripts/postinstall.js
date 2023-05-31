a255a31426501666b0b7f6a6502f32a0f9e848b5
王月功 <wangyuegong@chinatelecom.cn>
2023-05-31 14:02:58 +0800

    chore：调整提示语脚本，改成正则匹配版本号

--- packages/authenticate/scripts/postinstall.js
+++ packages/authenticate/scripts/postinstall.js
@@ -3,8 +3,10 @@ const version = pkg.version
 
 // 对于需要提醒的版本，在这里进行配置
 const tips = {
-  '2.0.1':
+  '2\\.0\\.\\d':
     '@cutedesign/authenticate 已更新到 v2.0.x ，该版本调整了在对接 iam 、ctyun 时 layout 的初始化方式，及配置项 providers 部分字段。为了避免影响到使用，建议先阅读 README.md 使用说明第7点。',
 }
 
-tips[version] && console.log(tips[version])
+Object.keys(tips).forEach(v => {
+  new RegExp(v).test(version) && console.log(tips[v])
+})
